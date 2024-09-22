78152147b87504583f7d3d8c5c9a00f5f73f9ded
ComBba <cent@naver.com>
2024-09-22 17:16:35 +0900

    블럭기준 상향. 데이터베이스 커밋 간격 10,000으로 변경

--- snapshot_python/snapshot.py
+++ snapshot_python/snapshot.py
@@ -10,11 +10,11 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 # 필요한 상수 선언
 IPC_PATH = '/home/barahime/esn_services/chaindata/gesc.ipc'  # Parity 노드의 IPC 경로
-TARGET_BLOCK = 6000000  # 스냅샷을 찍을 블록 번호
+TARGET_BLOCK = 6409000  # 스냅샷을 찍을 블록 번호
 DATABASE_FILE = 'balances.db'  # SQLite 데이터베이스 파일명
 OUTPUT_JSON_FILE = 'balances.json'  # 최종 출력될 JSON 파일명
 ETHERSOCIAL_JSON_FILE = '/home/barahime/esn_services/parity-ethereum/ethcore/res/ethereum/ethersocial.json'  # 초기 accounts 파일 경로
-COMMIT_INTERVAL = 1000  # 데이터베이스 커밋 간격
+COMMIT_INTERVAL = 10000  # 데이터베이스 커밋 간격
 
 def main():
     # 시작 시간 기록
